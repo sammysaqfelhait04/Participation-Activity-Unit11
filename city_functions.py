d8eb2e8b799cce956545a5df10ec5e4e1bdeab49
sammy saqfelhait <ssaqfelhait@student.cscc.edu>
2026-04-03 17:38:10 -0400

    Update Unit 11 files

--- city_functions.py
+++ city_functions.py
@@ -1,5 +1,4 @@
 def city_country(city, country, population=None):
-    """Return a formatted string like City, Country, with optional population."""
     
     if population:
         return f"{city.title()}, {country.title()} - population {population}"
